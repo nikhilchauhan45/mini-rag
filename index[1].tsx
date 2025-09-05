2ab44731b57d255e97529debea46493767da0d15
nikhilchauhan45 <nikhilchauhan78965@gmail.com>
2025-09-05 10:30:30 +0530

    Add files via upload

---
+++ index[1].tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Mini RAG Project</h1>
+      <p>Deployment successful! Ab tu backend/frontend integrate kar sakta hai.</p>
+    </div>
+  );
+}
